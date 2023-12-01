9d19b275fb077cceb389114fac559edb702f815f
Santi <rodriguez.ivan.santiago8@gmail.com>
2023-11-30 21:46:56 -0300

    Challenge "StringEndsWith" ended.

--- Novice/StringEndsWith/src/Main.java
+++ Novice/StringEndsWith/src/Main.java
@@ -1,7 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        boolean result = StringEndsWith("abc", "bc");
+        System.out.println(result);
+        boolean result2 = StringEndsWith("abc", "cd");
+        System.out.println(result2);
 
     }
+
+    public static boolean StringEndsWith(String s1, String s2) {
+        return s1.endsWith(s2);
+    }
+
 }
